f4d46973930d735d0637d2de4cd9f2b1d5bfb87d
jonathan2406 <jonathan2406.garcia@gmail.com>
2024-06-06 16:24:52 -0500

    actualizacion de requerimientos, app e index para despliegue en heroku con puerto e ip variable

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-
 from flask_socketio import SocketIO, send
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +22,5 @@ def index():
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0")
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
