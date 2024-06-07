5b1d4a5e6e32191fa8a76969c5d1c28185e498be
jonathan2406 <jonathan2406.garcia@gmail.com>
2024-06-06 19:39:11 -0500

    como estaba antes

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
-from flask_cors import CORS
 import os
-
+from flask_cors import CORS
+#a
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://chatflask-ef559a6c0275.herokuapp.com"}})  # Configuración de CORS
+CORS(app)
 
-app.config['SECRET'] = "secret123"
+app.config['SECRET'] = "secret123";
 
-socketio = SocketIO(app, cors_allowed_origins="https://chatflask-ef559a6c0275.herokuapp.com")  # Configuración de SocketIO con CORS
+socketio = SocketIO(app, cros_allowed_orgins="*")
+
 
 @socketio.on('message')
 def handle_message(message):
-    print("Received Message: " + message)
+    print("Received Message: " + message);
     if message != "User Connected!":
         send(message, broadcast=True)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
